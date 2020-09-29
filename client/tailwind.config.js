46e89a1b82a272c4625b22becec28e8a4b4c9a07
David Nguyen <nguyendavid3322@gmail.com>
2020-09-29 14:46:39 -0400

    fix tailwindcss for netlify

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,12 +2,7 @@ module.exports = {
   purge: {
     // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
     enabled: process.env.NODE_ENV === "production",
-    content: [
-      "components/**/*.vue",
-      "layouts/**/*.vue",
-      "views/**/*.vue",
-      "plugins/**/*.js",
-    ],
+    content: ["./public/**/*.{js,vue}", "./src/**/*.{js,vue}"],
   },
   target: "relaxed",
   prefix: "",
