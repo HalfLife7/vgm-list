37f1d04cb412936d9f38655a0c0d1be15009cca8
David Nguyen <nguyendavid3322@gmail.com>
2020-09-30 00:29:37 -0400

    set cors

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 
-console.log(process.env);
-
 export default axios.create({
   baseURL: process.env.VUE_APP_BACKEND_URL,
   headers: {
