c6ed520f8042451e8de302b5364fc3cc8b18de0a
David Nguyen <nguyendavid3322@gmail.com>
2020-09-30 02:20:32 -0400

    test other fixes for css import

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,11 +6,11 @@ module.exports = {
       // this will apply to both plain `.scss` files
       // AND `<style lang="scss">` blocks in `.vue` files
       {
-        test: /\.scss$/,
-        use: ["vue-style-loader", "css-loader", "sass-loader"]
-      }
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader", "sass-loader"],
+      },
     ],
-    devtool: "source-map"
-  }
+    devtool: "source-map",
+  },
   // plugin omitted
 };
